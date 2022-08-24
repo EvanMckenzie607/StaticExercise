cae6f11306b5c1351b6cc1c888f09a7f62ee0cc0
Evan Mckenzie <615evan@gmail.com>
2022-08-24 10:05:39 -0400

    CompletedExercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using StaticExercise;
+
+
+Console.WriteLine("Hello whats the tempature in Fareneit outside?!");
+
+var farenheitTemp = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Lets convert that to Celsius!");
+
+
+Console.WriteLine($"{farenheitTemp} " +
+    $"degrees Ferenheit is " +
+    $"{TempConverter.FahrenheitToCelsius(farenheitTemp)}" +
+    $" degrees" +
+    $" Celsius!");
+
+Console.WriteLine("Now lets convert a tempature to celsius!\n" +
+    "Please feed the console a number :)");
+
+var celsiusTemp = Double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{celsiusTemp} degrees Celsius is " +
+    $"{TempConverter.CelsiusToFarenheit(celsiusTemp)} " +
+    $"degrees" +
+    $"Farenheit!");
+
+
+
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double farenheitTemp)
+        {
+            var convertF = (farenheitTemp - 32) / 1.8;
+            var tempRounded = Math.Round(convertF, 2);
+            return tempRounded;
+        }
+        public static double CelsiusToFarenheit(double celsiusTemp)
+        {
+            var convertC = (celsiusTemp * 9) / 5 + 32;
+            var tempRounded = Math.Round(convertC, 2);
+            return tempRounded;
+        }
+    }
+
+}
